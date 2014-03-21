b257b5e769a53dde21303b66980725d141eee2c3
cdangg <dang.celia@gmail.com>
2014-03-20 23:34:33 -0400

    add subscription mailer

--- app/controllers/subscribers_controller.rb
+++ app/controllers/subscribers_controller.rb
@@ -1,2 +1,11 @@
 class SubscribersController < ApplicationController
+  def new
+    @subscriber = Subscriber.new
+  end
+
+  def create
+    @subscriber = Subscriber.new(params[:subscriber])
+    @subscriber.save
+    redirect_to root_url
+  end
 end
---
+++ app/mailers/subscription_mailer.rb
@@ -0,0 +1,3 @@
+class SubscriptionMailer < ActionMailer::Base
+  default from: "from@example.com"
+end
