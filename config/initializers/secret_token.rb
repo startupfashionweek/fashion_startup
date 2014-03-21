6ca87712eab77fb2a17c764fed1ab2b06b1b04e2
cdangg <dang.celia@gmail.com>
2014-03-20 22:25:17 -0400

    add welcome controller

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FashionStartup::Application.config.secret_key_base = '30b7623364b656e6e8fb92b60bbb8f45baed11f6f3c4d632649864d868ea664a487df244c1cb1cdf8b11eb1df0f2390e91d826f574fe4f2075f2d0bbe68f6696'
