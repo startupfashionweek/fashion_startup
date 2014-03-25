2eec84e2c51b91e90607c3f8e200a5fd7b33c070
cdangg <dang.celia@gmail.com>
2014-03-25 17:52:30 -0400

    modify volunteer mailer to send better emails

--- app/mailers/volunteer_mailer.rb
+++ app/mailers/volunteer_mailer.rb
@@ -1,6 +1,6 @@
 class VolunteerMailer < ActionMailer::Base
   default from: "volunteer@startupfashionweek.com"
-  default :bcc => "volunteer@startupfashionweek.com"
+  # default :bcc => "volunteer@startupfashionweek.com"
   def send_email(volunteer)
     @volunteer = volunteer
     mail(to: volunteer.email, subject: 'Welcome to Fashion Startup')
