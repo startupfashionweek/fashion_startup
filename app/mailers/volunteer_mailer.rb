cc142372de4cb59cdcc1962558a548a77fb72490
cdangg <dang.celia@gmail.com>
2014-03-25 17:35:23 -0400

    create form for new volunteers and set up volunteer mailer

--- app/mailers/volunteer_mailer.rb
+++ app/mailers/volunteer_mailer.rb
@@ -1,3 +1,8 @@
 class VolunteerMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "volunteer@startupfashionweek.com"
+  default :bcc => "volunteer@startupfashionweek.com"
+  def send_email(volunteer)
+    @volunteer = volunteer
+    mail(to: volunteer.email, subject: 'Welcome to Fashion Startup')
+  end
 end
