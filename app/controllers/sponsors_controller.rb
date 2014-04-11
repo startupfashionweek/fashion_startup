3cfbfb3232e399456bd2affcf2fbcc342a3846da
cdangg <dang.celia@gmail.com>
2014-04-10 20:46:37 -0400

    edit sponsor controller

--- app/controllers/sponsors_controller.rb
+++ app/controllers/sponsors_controller.rb
@@ -1,2 +1,13 @@
 class SponsorsController < InheritedResources::Base
+  def index
+    @sponsors = Sponsors.all
+  end
+
+  def show
+    @sponsor = Sponsor.find(params[:id])
+  end
+
+  def edit
+    @sponsor = Sponsor.find(params[:id])
+  end
 end
