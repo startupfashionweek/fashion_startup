a9c13aaae42c7795c767a5c2801470b4a9518d49
cdangg <dang.celia@gmail.com>
2014-06-10 14:25:01 -0700

    Add CMS to interested_startup and fix upload errors for logo and cover_image

--- app/mailers/startup_mailer.rb
+++ app/mailers/startup_mailer.rb
@@ -2,6 +2,7 @@ class StartupMailer < ActionMailer::Base
   default :from => "info@startupfashionweek.com"
   def send_email(interested_startup)
     @interested_startup = interested_startup
+    attachments['filename.jpg'] = {:mime_type => ''}
     mail(to: interested_startup.email, subject: 'Welcome to Fashion Startup')
   end
 end
--- app/models/interested_startup.rb
+++ app/models/interested_startup.rb
@@ -1,4 +1,6 @@
 class InterestedStartup < ActiveRecord::Base
+  mount_uploader :logo, LogoUploader
+  mount_uploader :cover_image, CoverImageUploader
   validates_presence_of :name, :contactname, :email
   validates_uniqueness_of :name
 end
