2efedf07abef4cfe5ab3618ee40bafdfed115079
cdangg <dang.celia@gmail.com>
2014-06-19 14:04:31 -0700

    mount uploader to all_sponsor.rb

--- app/models/all_sponsor.rb
+++ app/models/all_sponsor.rb
@@ -1,2 +1,7 @@
 class AllSponsor < ActiveRecord::Base
+  mount_uploader :platinum, PlatinumUploader
+  mount_uploader :gold, GoldUploader
+  mount_uploader :silver, SilverUploader
+  mount_uploader :bronze, BronzeUploader
+  mount_uploader :partners, PartnersUploader
 end
