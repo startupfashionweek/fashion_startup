65b27ff3a0f418b04b719653f34132ed11545a17
cdangg <dang.celia@gmail.com>
2014-06-27 10:27:07 -0700

    change event path in welcome page to event_spotlight

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,5 +3,6 @@ class WelcomeController < ApplicationController
     @subscriber = Subscriber.new
     @featured = Spotlight.find(1).currently_featured
     @sponsor = SponsorSpotlight.find(1).sponsor_featured
+    @event = EventSpotlight.find(1)
   end
 end
