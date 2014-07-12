110be6570941bb0763c21f07c8b4bee77f5651ac
cdangg <dang.celia@gmail.com>
2014-07-12 01:31:33 -0400

    fix typo on validations

--- app/models/sponsor.rb
+++ app/models/sponsor.rb
@@ -3,4 +3,5 @@ class Sponsor < ActiveRecord::Base
   mount_uploader :cover_image, CoverImageUploader
 
   vaildates_presence_of :name, :description, :url, :cover_image, :logo
+  vaildates_uniqueness_of :name
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -1,6 +1,8 @@
 class Team < ActiveRecord::Base
   mount_uploader :picture, PictureUploader
 
+  vaildates_presence_of :name, :role, :picture
+
   scope :management_team, -> { where(status: 'management team')}
   scope :advisory_board, -> { where(status: 'advisory board')}
 
