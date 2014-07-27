9a759fb471002a7c823b800b2a58a0e84d613c84
Bui Quang Duy <vntopcoders@gmail.com>
2014-07-27 09:51:36 +0700

    Update by rubytopcoders@gmail.com

--- app/models/media.rb
+++ app/models/media.rb
@@ -5,4 +5,5 @@ class Media < ActiveRecord::Base
   scope :media_inquires, -> { where(select_one: 'media inquires')}
   scope :press_releases, -> { where(select_one: 'press releases')}
   scope :media_partners, -> { where(select_one: 'media partners')}
+  scope :media_coverage, -> { where(select_one: 'media coverage')}
 end
--- app/models/participant.rb
+++ app/models/participant.rb
@@ -2,11 +2,11 @@ class Participant < ActiveRecord::Base
   mount_uploader :headshot, HeadshotUploader
   validates_presence_of :title, :name, :headshot
 
-  scope :conference_speakers, -> { where(title: 'conference speakers')}
-  scope :digital_fashion_judges, -> { where(title: 'digital fashion judges')}
-  scope :digital_fashion_entrepreneurs, -> { where(title: 'digital fashion entrepreneurs')}
-  scope :workshop_1_speakers, -> { where(title: 'workshop 1 speakers')}
-  scope :workshop_2_speakers, -> { where(title: 'workshop 2 speakers')}
+  scope :conference_speakers, -> { where(title: 'oct. 8 -conference speakers')}
+  scope :digital_fashion_demo_presenters, -> { where(title: 'oct. 7 -digital fashion demo presenters')}
+  scope :startup_emerging_designers, -> { where(title: 'Oct. 9 -Startup/Emerging Designers')}
+  scope :digital_fashion_startups, -> { where(title: 'Oct. 9 -Digital Fashion Startups')}
+  scope :exhibitors_lounge_partners, -> { where(title: 'Oct. 9 -Exhibitors/Demo Lounge Partners')}
   scope :workshop_3_speakers, -> { where(title: 'workshop 3 speakers')}
   scope :fashion_show_designers, -> { where(title: 'fashion show designers')}
   scope :fashion_show_judges, -> { where(title: 'fashion show judges')}
