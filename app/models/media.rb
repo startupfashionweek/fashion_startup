f75900bcfdbc3354adf7071c56af37e0e32f8099
Bui Quang Duy <vntopcoders@gmail.com>
2014-07-29 06:29:50 +0700

    update by rubytopcoders@gmail.com

--- app/models/media.rb
+++ app/models/media.rb
@@ -1,7 +1,7 @@
 class Media < ActiveRecord::Base
   mount_uploader :pdf_files, PdfFilesUploader
   mount_uploader :cover_image, PictureUploader
-  validates_presence_of :select_one, :content
+  validates_presence_of :select_one
 
   scope :media_inquires, -> { where(select_one: 'media inquires')}
   scope :press_releases, -> { where(select_one: 'press releases')}
